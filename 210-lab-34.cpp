e933fce8f01b8e2d581979d08af6f29813fdef8c
7Bssk7 <arhip.fin3@gmail.com>
2025-11-23 18:24:33 -0800

    Update

--- 210-lab-34.cpp
+++ 210-lab-34.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 const int SIZE = 12; 
@@ -34,9 +35,51 @@ public:
             cout << endl;
         }
     }
+
+    vector<int> bfs(int src) {
+        int V = adjList.size();
+        vector<bool> visited(V, false);
+        vector<int> res;
+    
+        queue<int> q;
+    
+        visited[src] = true;
+        q.push(src);
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            res.push_back(curr);
+
+            for (auto &x : adjList[curr]) {
+                int next = x.first;
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.push(next);
+                }
+            }
+        }
+    
+    return res;
+    }
+    
 };
 
 int main(){
+    vector<Edge> edges = {
+        {0, 1, 50},    // SF -> SJ
+        {0, 2, 90},    // SF -> SAC
+        {2, 9, 580},   // SAC -> PDX
+        {9, 8, 170},   // PDX -> SEA
+        {1, 3, 340},   // SJ -> LA
+        {3, 4, 120},   // LA -> SD
+        {3, 5, 270},   // LA -> LS
+        {4, 6, 355},   // SD -> PHX
+        {6, 7, 115},   // PHX -> TUS
+        {6, 5, 300},   // PHX -> LS
+        {5, 10, 420},  // LS -> SLC
+        {10, 11, 520}  // SLC -> DEN
+    };
 
     return 1;
 
